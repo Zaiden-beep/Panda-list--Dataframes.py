453c33f92de2e250f22959b8bf5d9cc596361c78
Zaid Ndabeni <54586612+Zaiden-beep@users.noreply.github.com>
2020-07-03 10:00:38 +0200

    Add files via upload

---
+++ Py.Project Sprint 5/Dataframe.py
@@ -0,0 +1,14 @@
+# importing pandas as pd 
+import pandas as pd 
+
+# Create the dataframe 
+df = pd.DataFrame({'Chips':['Simba', 'Lays',''], 
+					'Cooldrinks':['Coke', 'Fanta', ''], 
+					'Chocolates':['Cadbury', 'Tex', ''],
+                    'Pies':['Pepper Steak', 'Chicken', ''],
+                    'Fruit':['Pear', 'Apple', 'Orange'],
+                    'Cupcakes':['Vanilla', 'Chocolate', ''],
+                    'Veggies':['Spinach', 'Cabbage', '']}) 
+
+# Print the dataframe 
+print(df) 
